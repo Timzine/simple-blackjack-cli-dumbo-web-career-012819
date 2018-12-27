857bcc617b4b1182a72fdc66ec1cae53dafaad17
Lemrabt Boudadya <lboudadya@gmail.com>
2018-12-27 21:18:30 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -42,9 +42,9 @@ def hit(card_total)
   # code hit? here
   prompt_user
   user_input = get_user_input
-   if user_input = "s" 
+   if user_input = "h"   
      card_total += deal_card
-   elsif user_input = "h"
+   elsif user_input = "s"
      card_total
     else  
       invalid_command
