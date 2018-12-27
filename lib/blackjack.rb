01faab45865021b8faeec3439cb6f515f96dc56e
Lemrabt Boudadya <lboudadya@gmail.com>
2018-12-27 21:10:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -41,7 +41,9 @@ end
 def hit(card_total)
   # code hit? here
   prompt_user
-  get_user_input
+  user_input = get_user_input
+   if user_input = "s" then 
+   elsif user_input = "h"then deal_card
   
   
 end
